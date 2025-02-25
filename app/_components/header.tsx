33d549c8c7da02390502f76eb1ac309912fafd21
waris <m.waris390@gmail.com>
2025-02-25 17:35:12 +0500

    attempt 5 remove dead code from files

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { usePathname } from "next/navigation";
 
-export default function header(user: {
+export default function Header(user: {
   user:
     | {
         message: string;
